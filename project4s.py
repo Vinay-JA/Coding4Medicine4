c6291f1ee9ba95298ddc026c308c018cf6ef25c3
Vinay-JA <72953657+Vinay-JA@users.noreply.github.com>
2024-06-06 10:43:40 -0700

    Add files via upload

---
+++ project4s.py
@@ -0,0 +1,36 @@
+import re
+
+
+def countRestrictionSites(genomesequence, recognitionsequence):
+    recognitionpattern = recognitionsequence.replace('N','[ATCG]')       
+    matches = re.findall(recognitionpattern,genomesequence)
+    return len(matches)
+
+def findrestrictionsites(genomesequence,recognitionsequence):
+    recognitionpattern = recognitionsequence.replace('N','[ATCG]')
+    pattern = re.compile(recognitionpattern)
+    matches = [match.start() for match in re.finditer(pattern,genomesequence)]
+    return matches
+def findfragmentlengths(genomesequence,recognitionsequence):
+    cutsites = findrestrictionsites(genomesequence,recognitionsequence)
+    cutsites = sorted(cutsites)
+    cutpositions = [0] + cutsites + [len(genomesequence)]
+    fragmentlength = [
+        cutpositions[i+1] - cutpositions[i] for i in range(len(cutpositions)-1)
+    ]
+    return fragmentlength
+def readsequence(filepath):
+    with open(filepath, 'r') as file:
+        genomesequence = file.read().replace('\n','')
+    return genomesequence
+def main():
+    filepath = '/Users/vinayjagtiani/Code/Coding4Medicine/Project 4/Text.txt'
+    genomesequence = readsequence(filepath)
+    recognitionsequence = "GACNNNNNGTC"
+    numsites = countRestrictionSites(genomesequence,recognitionsequence)
+    numfragments = numsites + 1
+    print(f"The enzyme EclHKI will split the genome into {numfragments} parts.")
+    fragmentlength = findfragmentlengths(genomesequence,recognitionsequence)
+    print(f"Fragment lengths: {fragmentlength}")
+if __name__=="__main__":
+    main()
